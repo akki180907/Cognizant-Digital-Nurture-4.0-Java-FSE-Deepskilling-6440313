e2badc9b6df53072fe63017e5703f14ca5e25f2f
akki180907 <147834149+akki180907@users.noreply.github.com>
2025-07-26 19:45:21 +0530

    Add files via upload

---
+++ Week 6/6.ReactJS-HOL/trainersapp/src/TrainersMock.js
@@ -0,0 +1,9 @@
+import Trainer from './Trainer';
+
+const trainers = [
+  new Trainer(101, "Akshaya", "akshaya@example.com", "1234567890", "React", "React, JS, HTML"),
+  new Trainer(102, "Sriram", "sriram@example.com", "0987654321", "Java", "Java, Spring, Hibernate"),
+  new Trainer(103, "Nisha", "nisha@example.com", "1112223333", "Python", "Python, Django"),
+];
+
+export default trainers;
